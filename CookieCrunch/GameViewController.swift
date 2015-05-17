4f37c1c9d4963a61ff88231fc183c17cc2ea7079
Jake Scott <jake.net@gmail.com>
2015-05-17 16:57:11 +1200

    Init

---
+++ CookieCrunch/GameViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+import SpriteKit
+
+class GameViewController : UIViewController {
+    var scene: GameScene!
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        skView.showsFPS = true
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the scene.
+        skView.presentScene(scene)
+    }
+}
