bb0179327cba866d8884987f8ffca5cfe9c4eb4e
Jake Scott <jake.net@gmail.com>
2015-05-17 17:42:15 +1200

    Add cookies to the game scene

--- CookieCrunch/GameViewController.swift
+++ CookieCrunch/GameViewController.swift
@@ -3,6 +3,7 @@ import SpriteKit
 
 class GameViewController : UIViewController {
     var scene: GameScene!
+    var level: Level!
     
     override func prefersStatusBarHidden() -> Bool {
         return true
@@ -28,7 +29,21 @@ class GameViewController : UIViewController {
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
         
+        level = Level()
+        scene.level = level
+        
         // Present the scene.
         skView.presentScene(scene)
+        
+        beginGame()
+    }
+    
+    func beginGame() {
+        shuffle()
+    }
+    
+    func shuffle() {
+        let newCookies = level.shuffle()
+        scene.addSpritesForCookies(newCookies)
     }
 }
